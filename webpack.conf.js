ec5b993359d081309628ff6efd239ffd4a851e96
root <root@LAPTOP-2PALO9HE.localdomain>
2020-10-11 08:40:30 +0800

    login

---
+++ webpack.conf.js
@@ -0,0 +1,14 @@
+module: {
+
+        rules: [
+
+            {
+
+                test: /\.less$/,
+
+                loader: "style-loader!css-loader!less-loader"
+
+            }
+
+        ]
+    }
